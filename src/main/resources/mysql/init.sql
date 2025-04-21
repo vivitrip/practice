8e5fde7a05f70f85a88dcfde4fd7f28235fb9853
Vi_error <vivitrip@163.com>
2025-04-21 14:43:40 +0800

    环境准备

---
+++ src/main/resources/mysql/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE test_user (
+                           id INT PRIMARY KEY,
+                           name VARCHAR(100),
+                           age INT
+);
+
+INSERT INTO test_user (id, name, age) VALUES
+                                          (1, 'Alice', 25),
+                                          (2, 'Bob', 30),
+                                          (3, 'Charlie', 22),
+                                          (4, 'Daniel', 32),
+                                          (5, 'Edison', 18)
+;
